63a8b06812cc4fa6e5f590404dbc4c5c28f998e3
Muhammad abdullah <83001128+Abdullah-2001@users.noreply.github.com>
2023-08-09 00:33:47 -0700

    commit by abdullah

---
+++ Loops/app.js
@@ -0,0 +1,30 @@
+// FOR LOOPS
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for (let index = 0; index < arr.length; index++) {
+//     const numbers = arr[index];
+//     console.log(numbers);
+// }
+
+// WHITE LOOPS
+
+// While loop works same as for loop
+
+// var i = 0;
+// while (i < 0) {
+//     i++;
+//     console.log(i);
+// }
+
+// DO WHILE LOOPS
+
+// Do While loop print the statement first then check the condition
+
+// var i = 0;
+
+// do {
+//     console.log(i);
+//     i++;
+//     console.log(i);
+// } while (i < 5);
